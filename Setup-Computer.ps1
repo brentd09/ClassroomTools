bd907229f40d23187d190591a62012bb1e6c5999
Brent Denny <brent.denny@ddls.com.au>
2019-03-06 16:19:47 +1100

    room setups nearly complete

--- Setup-Computer.ps1
+++ Setup-Computer.ps1
@@ -1,46 +1,54 @@
 <#
 .SYNOPSIS
-  Short description
+  Configure my Instructor Machines ready for use
 .DESCRIPTION
-  Long description
+  This will: 
+    Update Windows defender
+    Install the latest version of VSCode
+    Install the lasest version of Git
+    Setup my VSCode preferences
+    Setup my Git configs
+    Create a Git root 
 .EXAMPLE
-  PS C:\> <example usage>
-  Explanation of what the example does
-.INPUTS
-  Inputs (if any)
-.OUTPUTS
-  Output (if any)
+  Setup-Computer
 .NOTES
   General notes
+  Created By: Brent Denny
+  Created On: 6 Mar 2019
 #>
 [CmdletBinding()]
 Param()
 $WinUpdate = Get-Service -Name wuauserv
 
 if ($WinUpdate.StartType -ne 'Manual') {Set-Service -Name wuauserv -StartupType Manual}
-if ($WinUpdate.Status -ne 'Running') {Start-Service -Name wuauserv}
-if ($WinUpdate.Status -eq 'Running') {Update-MpSignature}
-Start-MpScan
+if ($WinUpdate.Status -ne 'Running') {
+  Start-Service -Name wuauserv
+  Update-MpSignature
+}
+if ( ((get-date) - (Get-MpComputerStatus).QuickScanEndTime).hours -gt 12 ) {Start-MpScan}
 Stop-Service -Name wuauserv
 
 [net.servicePointManager]::SecurityProtocol = [net.SecurityProtocolType]::Tls12
-$url = 'https://aka.ms/win32-x64-user-stable'
-$FilePath = "$home/downloads/InstallVSCode.exe"
-
+$VSCodeUrl = 'https://aka.ms/win32-x64-user-stable'
+$VSCodeFilePath = "$home/downloads/InstallVSCode.exe"
+$GitUrl = "https://github.com/git-for-windows/git/releases/download/v2.21.0.windows.1/Git-2.21.0-64-bit.exe"
+$GitFilePath = "$home/downloads/Git64.exe"
 $WebClient = New-Object -TypeName System.Net.WebClient
-$WebClient.DownloadFile($url,$FilePath)
+$WebClient.DownloadFile($VSCodeurl,$VSCodeFilePath)
+$WebClient.DownloadFile($GitUrl,$GitFilePath)
 
-Execute-Process -Path "$FilePath" -Parameters "/VERYSILENT /CLOSEAPPLICATIONS"
-
-Start-Process -FilePath "$env:LOCALAPPDATA\Programs\Microsoft VS Code\Code.exe"
+Invoke-Expression -Command "$VSCodeFilePath /VERYSILENT " 
+Invoke-Expression -Command "$GitFilePath /VERYSILENT " 
+$currentLocation = Get-Location
+Set-Location ("$env:LOCALAPPDATA\Programs\Microsoft VS Code\")
+Invoke-Expression -Command (".\Code.exe")
 Start-Sleep -Seconds 2
-$CodeProcesses = get-process -Name Code
-$CodeProcesses | ForEach-Object {$_.CloseMainWindow() *> $null}
+
 
 # attempting to copy my UserSettings and Snippets into VSCode
 try {
-  Copy-Item .\settings.json $env:APPDATA\Code\User\ -ErrorAction stop
-  Copy-Item .\powershell.json $env:APPDATA\Code\User\snippets\ -ErrorAction Stop
+  Copy-Item $currentLocation\settings.json $env:APPDATA\Code\User\ -ErrorAction stop
+  Copy-Item $currentLocation\powershell.json $env:APPDATA\Code\User\snippets\ -ErrorAction Stop
 }
 catch {Write-Warning "File copy did not work"}
 
@@ -51,3 +59,4 @@ try {
 catch {
   Write-Warning 'The git command did not work'
 }
+Set-Location $currentLocation.Path
