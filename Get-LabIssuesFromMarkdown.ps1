2d6f0649aa896e5e85a428c22a488317282e1dee
Brent Denny <brent.denny@ddls.com.au>
2020-05-29 22:28:49 +1000

    name change

---
+++ Get-LabIssuesFromMarkdown.ps1
@@ -0,0 +1,10 @@
+﻿$files = Get-ChildItem -Path .\ -file | Where-Object {$_.name -like 'mod*'}
+foreach ($file in $files) {
+  $Issue = get-content -Path $file.FullName   | Select-String -Pattern '^## Lab Issues' 
+  $StartOfIssue = $Issue.LineNumber
+  $endOfFile =  (get-content $file).count
+  $TailLines = $endOfFile - $StartOfIssue + 1
+  Write-Host $file.Name
+  Get-Content $file -Tail $TailLines
+  Write-Host '-------------------------------------'
+}
