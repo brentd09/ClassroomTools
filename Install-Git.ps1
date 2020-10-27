29318fe8f55f43c75db6ef0ab79edb8046592a0f
Administrator@ADATUM.COM <Administrator@ADATUM.COM>
2020-10-27 07:28:50 -0700

    Install git script

---
+++ Install-Git.ps1
@@ -0,0 +1,11 @@
+﻿$GitDownloadSite = Invoke-WebRequest -Uri 'https://git-scm.com/download/win' -UseBasicParsing 
+$RawLink = ($GitDownloadSite.Links | where {$_ -like '*64-bit Git for Windows Setup*'}).OuterHtml
+$DownloadUrl = $RawLink -replace '^.+"(.+)".+$','$1'
+$DownloadsDir = ([System.Environment]::GetFolderPath('desktop')) -replace 'desktop','downloads'
+$GitExeFile = $DownloadUrl -replace '.+\/',''
+$GitExeFilePath = $DownloadsDir + '\' + $GitExeFile
+$WebClientObj = [System.Net.WebClient]::new()
+$WebClientObj.DownloadFile($DownloadUrl,$GitExeFilePath)
+
+$InstallString = "$GitExeFilePath /VERYSILENT /NORESTART"
+Invoke-Expression -Command $InstallString
