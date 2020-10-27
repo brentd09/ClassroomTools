0d34ba15e114138e0ff935c85f243bd4da6dec9c
brent denny <brent.denny@ddls.com.au>
2020-10-28 00:35:07 +1000

    minor changes to  Git installer

--- Install-Git.ps1
+++ Install-Git.ps1
@@ -1,9 +1,12 @@
-﻿$GitDownloadSite = Invoke-WebRequest -Uri 'https://git-scm.com/download/win' -UseBasicParsing 
+﻿#
+# Quick and dirty unattended Git installer for classes
+#
+$GitDownloadSite = Invoke-WebRequest -Uri 'https://git-scm.com/download/win' -UseBasicParsing 
 $RawLink = ($GitDownloadSite.Links | where {$_ -like '*64-bit Git for Windows Setup*'}).OuterHtml
 $DownloadUrl = $RawLink -replace '^.+"(.+)".+$','$1'
 $DownloadsDir = ([System.Environment]::GetFolderPath('desktop')) -replace 'desktop','downloads'
 $GitExeFile = $DownloadUrl -replace '.+\/',''
-$GitExeFilePath = $DownloadsDir + '\' + $GitExeFile
+$GitExeFilePath = $DownloadsDir.TrimEnd('\') + '\' + $GitExeFile
 $WebClientObj = [System.Net.WebClient]::new()
 $WebClientObj.DownloadFile($DownloadUrl,$GitExeFilePath)
 
